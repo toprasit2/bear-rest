4c367d37a87894734a6bbfe9864cfba5500cc31b
toprasit2 <toprasit.gk@gmail.com>
2018-04-22 20:04:13 +0700

    server support deleting

--- server/src/index.js
+++ server/src/index.js
@@ -1,24 +1,29 @@
 var express = require('express'); 
 var app = express(); 
 var router = express.Router(); 
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser'); 
 var cors = require('cors');
   
 var bears = [
-    { id: 1, name: 'Ple' },
-    { id: 2, name: 'Tum' }
+    { id: '1', name: 'Ple' },
+    { id: '2', name: 'Tum' }
 ]; 
   
 router.route('/bears') 
     .get(function(req, res) {
         res.send(bears);
     })
-    .post(function(req, res) {
+    .post(function(req, res) { 
         var bear = {}; 
         bear.name = req.body.name; 
         bears.push(bear); 
         res.json({ message: 'Bear created!' }); 
-    }); 
+    })
+router.route('/bears/:id')
+    .delete(function(req, res){
+        bears = bears.filter(b => b.id !== req.params.id)
+        res.json({ message: 'Bear deleted!' }); 
+    }) 
 
 app.use(cors());
 // all of our routes will be prefixed with /api 
