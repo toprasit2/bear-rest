a5078eb5a64895e9ea6b9f0976389675816b4563
toprasit2 <toprasit.gk@gmail.com>
2018-04-22 15:59:57 +0700

    list bears

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 var express = require('express'); 
 var app = express(); 
 var router = express.Router(); 
-var bodyParser = require('body-parser'); 
+var bodyParser = require('body-parser');
 var cors = require('cors');
   
 var bears = [
@@ -13,7 +13,7 @@ router.route('/bears')
     .get(function(req, res) {
         res.send(bears);
     })
-    .post(function(req, res) {  
+    .post(function(req, res) {
         var bear = {}; 
         bear.name = req.body.name; 
         bears.push(bear); 
